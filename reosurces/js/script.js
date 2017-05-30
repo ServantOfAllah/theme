41fddd99a6e2d3e78e18414625607b3795f724c8
ServantOfAllah <mojeed4@yahoo.com>
2017-05-30 18:51:10 +0400

    added jquery

---
+++ reosurces/js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $('.js-section-feature').waypoint(function(direction){
+        if(direction == 'down'){
+            $('nav').addClass('sticky');
+        }else{
+            $('nav').removeClass('sticky');
+        }
+    },{
+        offset: '60px;'
+    })
+    
+    
+    $('.js-scroll-to-plan').click(function(){
+        $('html, body').animate({scrollTop: $('.js-section-plan').offset().top}, 2500);
+    });
+    
+    $('.js-scroll-to-start').click(function(){
+        $('html, body').animate({scrollTop: $('.js-section-feature').offset().top}, 1000);
+    });
+    
+    $(function(){
+       $('a[href*=#]:not([href=#])').click(function(){
+           if(location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname){
+               var target = $(this.hash);
+               target = target.length ? target : $('[name' + this.hash.slice(1) +']');
+               if(target.length){
+                   $('html, body').animate({scrollTop: target.offset().top}, 1000);
+                   return false;
+               }
+           }
+       });
+    });
+    
+    
+}); 
